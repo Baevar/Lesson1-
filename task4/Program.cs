efdd35975ae07838dd4e1b03caeb2249d19321ed
baevar_laptop <baevar@mai.ru>
2024-03-14 14:15:26 +0700

    create lesson1:task4

---
+++ task4/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 7;
+int b = 4;
+int c = 3;
+int d = 21;
+int e = 5;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
