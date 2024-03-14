afbdc55fb3477a70248b02875cf53ec99e0fbbf3
baevar_laptop <baevar@mai.ru>
2024-03-14 10:20:57 +0700

    create variable lesson1:task1

--- task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e917db557b5d8da20c15161ef844ce53feff5377")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6f8f3adfbbc634904022ead9354a3b67864bc15")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
